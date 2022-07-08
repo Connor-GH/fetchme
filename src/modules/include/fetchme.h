18b7487299c440290703217d76031f502c7f34ba
Connor-GH <72793802+Connor-GH@users.noreply.github.com>
2022-07-08 00:36:51 +0000

    Add files via upload

---
+++ src/modules/include/fetchme.h
@@ -0,0 +1,14 @@
+int cpu_info();
+int disk();
+int distro();
+int gpu_info();
+int kernel();
+int memory_info();
+int package_count();
+int refresh_rate();
+int resolution();
+int shell();
+int terminal(char* const* envp);
+int uptime_info();
+int username_at_hostname();
+int wm();
