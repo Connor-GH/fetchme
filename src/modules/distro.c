9d567fd68043231037d7079673f9e6e58662b9f4
Connor <connor-gh@outlook.com>
2022-08-17 17:05:38 -0500

    Changed distro info to use PRETTY_NAME instead of NAME

--- src/modules/distro.c
+++ src/modules/distro.c
@@ -11,6 +11,7 @@
     struct utsname buffer;
 
     char os_name[50];
+    char info_desc[20];
 
     FILE *os_release = fopen("/etc/os-release", "r");
 
@@ -24,20 +25,34 @@
         exit(EXIT_FAILURE);
     }
 
-    fscanf(os_release, "NAME=%49[^\n]+", os_name);  // get everything
+    while (fgets(os_name, sizeof os_name, os_release) != NULL) {
+
+        sscanf(os_name, "%19[^=]=\"%49[^\"]", info_desc, os_name);
+        if (strcmp(info_desc, "PRETTY_NAME") == 0) { 
+
+            fclose(os_release);
+            printf("%sOS:\033[0m %s %s\n", \
+                color_distro(), os_name, buffer.machine);
+            return EXIT_SUCCESS;
+        }
+    }
+    fclose(os_release);
+    fprintf(stderr, "Distro name not found.\n");
+
+    ////fscanf(os_release, "NAME=%49[^n]+", os_name);  // get everything
                                                     // that isn't a
                                                     // newline
-    fclose(os_release);
+    ////fclose(os_release);
     /*
      * remove quotation marks from
      * os name if needed.
      */
-        sscanf(os_name, "\"%49[^\"]+", os_name);
+       //// sscanf(os_name, "\"%49[^\"]+", os_name);
 
-        printf("%sOS:\033[0m %s %s\n", \
+       //// printf("%sOS:\033[0m %s %s\n", \
                 color_distro(), os_name, buffer.machine);
 
 
 
-    return EXIT_SUCCESS;
+    return EXIT_FAILURE/*SUCCESS*/;
 }
