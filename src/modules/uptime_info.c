9b1910c8ef4cf43e577e761835daf5badf3d97ea
Connor-GH <connor-gh@outlook.com>
2024-11-17 12:17:04 -0600

    POSIX implementation for uptime info, and add support for day count

--- src/modules/uptime_info.c
+++ src/modules/uptime_info.c
@@ -1,40 +1,28 @@
+#include "./include/fetchme.h"
 #include <stdio.h>
 #include <stdlib.h>
-
-#include "./include/fetchme.h"
-
-#if BSD_SUPPORT_ONLY
 #include <time.h>
-#endif
+
 int
 uptime_info(const char *color_distro)
 {
-#if LINUX_SUPPORT_ONLY
-	char uptime[64];
 	int min = 0;
-	double uptime_f = 0;
-
-	FILE *up = fopen("/proc/uptime", "r");
-	if (up == NULL) {
-		perror("/proc/uptime");
-		exit(EXIT_FAILURE);
-	}
-	fscanf(up, "%63s %*63s", uptime);
-	fclose(up);
-
-	uptime_f = atof(uptime);
-
-	min = ((int)(uptime_f) / 60) % 60;
-	printf("%sUptime:\033[0m %d hours, %d mins\n", color_distro,
-		   (int)(uptime_f / 3600), min);
-#elif BSD_SUPPORT_ONLY
+	int hours = 0;
+	int days = 0;
 	struct timespec time_spec;
-	if (clock_gettime(CLOCK_UPTIME_PRECISE, &time_spec) != 0)
+	if (clock_gettime(CLOCK_BOOTTIME, &time_spec) != 0)
 		return EXIT_SUCCESS;
 
-	unsigned long uptime = time_spec.tv_sec;
-	printf("%sUptime:\033[0m %d hours, %d mins\n", color_distro,
-		   (int)(uptime / 3600), (int)(uptime / 60) % 60);
-#endif
+	time_t uptime = time_spec.tv_sec;
+
+	min = ((int)(uptime) / 60) % 60;
+	hours = (int)(uptime / 3600);
+	days = hours / 24;
+	hours %= 60;
+	printf("%sUptime:\033[0m ", color_distro);
+	if (days > 0)
+		printf("%d days, ", days);
+	printf("%d hours, %d mins\n", hours, min);
+
 	return EXIT_SUCCESS;
 }
